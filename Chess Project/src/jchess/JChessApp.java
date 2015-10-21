c8a4f2c3a379799fd38109310f00aec7f65ec936
Maurice <maurice.duvigneau@st.ovgu.de>
2015-10-21 18:27:10 +0200

    Add storing of config file at application shutdown

--- Chess Project/src/jchess/JChessApp.java
+++ Chess Project/src/jchess/JChessApp.java
@@ -22,35 +22,44 @@ import org.jdesktop.application.SingleFrameApplication;
  * The main class of the application.
  */
 public class JChessApp extends SingleFrameApplication {
-     static JChessView jcv;
-    /**
-     * At startup create and show the main frame of the application.
-     */
-    @Override protected void startup() {
-        jcv = new JChessView(this);
-        show(jcv);
-    }
+	static JChessView	jcv;
 
-    /**
-     * This method is to initialize the specified window by injecting resources.
-     * Windows shown in our application come fully initialized from the GUI
-     * builder, so this additional configuration is not needed.
-     */
-    @Override protected void configureWindow(java.awt.Window root) {
-    }
+	/**
+	 * At startup create and show the main frame of the application.
+	 */
+	@Override
+	protected void startup() {
+		jcv = new JChessView(this);
+		show(jcv);
+	}
 
-    /**
-     * A convenient static getter for the application instance.
-     * @return the instance of JChessApp
-     */
-    public static JChessApp getApplication() {
-        return Application.getInstance(JChessApp.class);
-    }
+	/**
+	 * This method is to initialize the specified window by injecting resources.
+	 * Windows shown in our application come fully initialized from the GUI
+	 * builder, so this additional configuration is not needed.
+	 */
+	@Override
+	protected void configureWindow(java.awt.Window root) {
+	}
 
-    /**
-     * Main method launching the application.
-     */
-    public static void main(String[] args) {
-        launch(JChessApp.class, args);
-    }
+	/**
+	 * A convenient static getter for the application instance.
+	 * @return the instance of JChessApp
+	 */
+	public static JChessApp getApplication() {
+		return Application.getInstance(JChessApp.class);
+	}
+
+	/**
+	 * Main method launching the application.
+	 */
+	public static void main(String[] args) {
+		launch(JChessApp.class, args);
+	}
+
+	@Override
+	protected void shutdown() {
+		GUI.storeConfigFile();
+		super.shutdown();
+	}
 }
