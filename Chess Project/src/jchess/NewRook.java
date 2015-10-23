8cf7f0113bded2d6628fbcdbbb26a37c57e21077
JingyiMa <majingyisunshine@gmail.com>
2015-10-23 13:30:07 +0200

    again modify NewRock

--- Chess Project/src/jchess/NewRook.java
+++ Chess Project/src/jchess/NewRook.java
@@ -4,202 +4,162 @@
 
 package jchess;
 
-import java.util.ArrayList;
-import java.awt.Graphics;
 import java.awt.Image;
+import java.util.ArrayList;
 //testtesttesttest
 
 /**
  * Class to represent a chess pawn rook
  * Rook can move:
  *       |_|_|_|X|_|_|_|_|7
-|_|_|_|X|_|_|_|_|6
-|_|_|_|X|_|_|_|_|5
-|_|_|_|X|_|_|_|_|4
-|X|X|X|B|X|X|X|X|3
-|_|_|_|X|_|_|_|_|2
-|_|_|_|X|_|_|_|_|1
-|_|_|_|X|_|_|_|_|0
-0 1 2 3 4 5 6 7
+         |_|_|_|X|_|_|_|_|6
+         |_|_|_|X|_|_|_|_|5
+         |_|_|_|X|_|_|_|_|4
+         |X|X|X|B|X|X|X|X|3
+         |_|_|_|X|_|_|_|_|2
+         |_|_|_|X|_|_|_|_|1
+         |_|_|_|X|_|_|_|_|0
+          0 1 2 3 4 5 6 7
  *
  */
-public class NewRook extends Rook
-{
+public class NewRook extends Rook {
 
-    boolean wasMotion = false;
-    protected static final Image imageWhite = GUI.loadImage("NewRook-W.png");
-    protected static final Image imageBlack = GUI.loadImage("NewRook-B.png");
-    public static short value = 5;
+	boolean												wasMotion		= false;
+	protected static final Image	imageWhite	= GUI.loadImage("NewRook-W.png");
+	protected static final Image	imageBlack	= GUI.loadImage("NewRook-B.png");
+	public static short						value				= 5;
 
-    NewRook(Chessboard chessboard, Player player)
-    {
-        super(chessboard, player);//call initializer of super type: Piece
-        //this.setImages("NewRook-W.png", "NewRook-B.png");
-        this.symbol = "R";
-        this.setImage();
-    }
+	NewRook(Chessboard chessboard, Player player) {
+		super(chessboard, player);// call initializer of super type: Piece
+		// this.setImages("NewRook-W.png", "NewRook-B.png");
+		this.symbol = "R";
+		this.setImage();
+	}
 
-    @Override
-    void setImage()
-    {
-        if (this.player.color == this.player.color.black)
-        {
-            image = imageBlack;
-        }
-        else
-        {
-            image = imageWhite;
-        }
-        orgImage = image;
-    }
+	@Override
+	void setImage() {
+		if (this.player.color == this.player.color.black) {
+			image = imageBlack;
+		} else {
+			image = imageWhite;
+		}
+		orgImage = image;
+	}
 
-    /**
-     *  Annotation to superclass Piece changing pawns location
-     * @return  ArrayList with new possition of piece
-     */
-    @Override
-    public ArrayList allMoves()
-    {
-        ArrayList list = new ArrayList();
+	/**
+	 *  Annotation to superclass Piece changing pawns location
+	 * @return  ArrayList with new position of piece
+	 */
+	@Override
+	public ArrayList allMoves() {
+		ArrayList list = new ArrayList();
 
-        for (int i = this.square.pozY + 1; i <= 5; ++i)
-        {//up
+		for (int i = this.square.pozY + 1; i <= 7; ++i) {// up
+			for (int step = 5; step > 0; --step) {
 
-            if (this.checkPiece(this.square.pozX, i))
-            {//if on this sqhuare isn't piece
+				if (this.checkPiece(this.square.pozX, i)) {// if on this square isn't
+																										// piece
 
-                if (this.player.color == Player.colors.white)
-                {//white
+					if (this.player.color == Player.colors.white) {// white
 
-                    if (this.chessboard.kingWhite.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[this.square.pozX][i]))
-                    {
-                        list.add(chessboard.squares[this.square.pozX][i]);
-                    }
-                }
-                else
-                {//or black
+						if (this.chessboard.kingWhite.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[this.square.pozX][i])) {
+							list.add(chessboard.squares[this.square.pozX][i]);
+						}
+					} else {// or black
 
-                    if (this.chessboard.kingBlack.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[this.square.pozX][i]))
-                    {
-                        list.add(chessboard.squares[this.square.pozX][i]);
-                    }
-                }
+						if (this.chessboard.kingBlack.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[this.square.pozX][i])) {
+							list.add(chessboard.squares[this.square.pozX][i]);
+						}
+					}
 
-                if (this.otherOwner(this.square.pozX, i))
-                {
-                    break;
-                }
-            }
-            else
-            {
-                break;//we've to break becouse we cannot go beside other piece!!
-            }
+					if (this.otherOwner(this.square.pozX, i)) {
+						break;
+					}
+				} else {
+					break;// we've to break becouse we cannot go beside other piece!!
+				}
 
-        }
+			}
+		}
 
-        for (int i = this.square.pozY - 1; i >= 0; --i)
-        {//down
+		for (int i = this.square.pozY - 1; i >= 0; --i) {// down
+			for (int step = 5; step > 0; --step) {
+				if (this.checkPiece(this.square.pozX, i)) {// if on this square isn't
+																										// piece
 
-            if (this.checkPiece(this.square.pozX, i))
-            {//if on this sqhuare isn't piece
+					if (this.player.color == Player.colors.white) {// white
 
-                if (this.player.color == Player.colors.white)
-                {//white
+						if (this.chessboard.kingWhite.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[this.square.pozX][i])) {
+							list.add(chessboard.squares[this.square.pozX][i]);
+						}
+					} else {// or black
 
-                    if (this.chessboard.kingWhite.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[this.square.pozX][i]))
-                    {
-                        list.add(chessboard.squares[this.square.pozX][i]);
-                    }
-                }
-                else
-                {//or black
+						if (this.chessboard.kingBlack.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[this.square.pozX][i])) {
+							list.add(chessboard.squares[this.square.pozX][i]);
+						}
+					}
 
-                    if (this.chessboard.kingBlack.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[this.square.pozX][i]))
-                    {
-                        list.add(chessboard.squares[this.square.pozX][i]);
-                    }
-                }
+					if (this.otherOwner(this.square.pozX, i)) {
+						break;
+					}
+				} else {
+					break;// we've to break because we cannot go beside other piece!!
+				}
+			}
+		}
 
-                if (this.otherOwner(this.square.pozX, i))
-                {
-                    break;
-                }
-            }
-            else
-            {
-                break;//we've to break becouse we cannot go beside other piece!!
-            }
-        }
+		for (int i = this.square.pozX - 1; i >= 0; --i) {// left
+			for (int step = 5; step > 0; --step) {
+				if (this.checkPiece(i, this.square.pozY)) {// if on this sqhuare isn't
+																										// piece
 
-        for (int i = this.square.pozX - 1; i >= 0; --i)
-        {//left
+					if (this.player.color == Player.colors.white) {// white
 
-            if (this.checkPiece(i, this.square.pozY))
-            {//if on this sqhuare isn't piece
+						if (this.chessboard.kingWhite.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[i][this.square.pozY])) {
+							list.add(chessboard.squares[i][this.square.pozY]);
+						}
+					} else {// or black
 
-                if (this.player.color == Player.colors.white)
-                {//white
+						if (this.chessboard.kingBlack.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[i][this.square.pozY])) {
+							list.add(chessboard.squares[i][this.square.pozY]);
+						}
+					}
 
-                    if (this.chessboard.kingWhite.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[i][this.square.pozY]))
-                    {
-                        list.add(chessboard.squares[i][this.square.pozY]);
-                    }
-                }
-                else
-                {//or black
+					if (this.otherOwner(i, this.square.pozY)) {
+						break;
+					}
+				} else {
+					break;// we've to break becouse we cannot go beside other piece!!
+				}
+			}
+		}
 
-                    if (this.chessboard.kingBlack.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[i][this.square.pozY]))
-                    {
-                        list.add(chessboard.squares[i][this.square.pozY]);
-                    }
-                }
+		for (int i = this.square.pozX + 1; i <= 7; ++i) {// right
+			for (int step = 5; step > 0; --step) {
+				if (this.checkPiece(i, this.square.pozY)) {// if on this sqhuare isn't
+																										// piece
 
-                if (this.otherOwner(i, this.square.pozY))
-                {
-                    break;
-                }
-            }
-            else
-            {
-                break;//we've to break becouse we cannot go beside other piece!!
-            }
-        }
+					if (this.player.color == Player.colors.white) {// white
 
-        for (int i = this.square.pozX + 1; i <= 5; ++i)
-        {//right
+						if (this.chessboard.kingWhite.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[i][this.square.pozY])) {
+							list.add(chessboard.squares[i][this.square.pozY]);
+						}
+					} else {// or black
 
-            if (this.checkPiece(i, this.square.pozY))
-            {//if on this sqhuare isn't piece
+						if (this.chessboard.kingBlack.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[i][this.square.pozY])) {
+							list.add(chessboard.squares[i][this.square.pozY]);
+						}
+					}
 
-                if (this.player.color == Player.colors.white)
-                {//white
+					if (this.otherOwner(i, this.square.pozY)) {
+						break;
+					}
+				} else {
+					break;// we've to break becouse we cannot go beside other piece!!
+				}
+			}
+		}
 
-                    if (this.chessboard.kingWhite.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[i][this.square.pozY]))
-                    {
-                        list.add(chessboard.squares[i][this.square.pozY]);
-                    }
-                }
-                else
-                {//or black
-
-                    if (this.chessboard.kingBlack.willBeSafeWhenMoveOtherPiece(this.square, chessboard.squares[i][this.square.pozY]))
-                    {
-                        list.add(chessboard.squares[i][this.square.pozY]);
-                    }
-                }
-
-                if (this.otherOwner(i, this.square.pozY))
-                {
-                    break;
-                }
-            }
-            else
-            {
-                break;//we've to break becouse we cannot go beside other piece!!
-            }
-        }
-
-        return list;
-    }
+		return list;
+	}
 }
-
--- Chess Project/src/jchess/Piece.java
+++ Chess Project/src/jchess/Piece.java
@@ -20,193 +20,169 @@
  */
 package jchess;
 
-
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
 Class to represent a piece (any kind) - this class should be extended to represent pawn, bishop etc.
  */
-public abstract class Piece
-{
+public abstract class Piece {
 
-    Chessboard chessboard; // <-- this relations isn't in class diagram, but it's necessary :/
-    public Square square;
-    public Player player;
-    String name;
-    protected String symbol;
-    protected static Image imageBlack;// = null;
-    protected static Image imageWhite;// = null;
-    public Image orgImage;
-    public Image image;
-    public static short value = 0;
+	Chessboard							chessboard;	// <-- this relations isn't in class
+																			// diagram, but it's necessary :/
+	public Square						square;
+	public Player						player;
+	String									name;
+	protected String				symbol;
+	protected static Image	imageBlack;	// = null;
+	protected static Image	imageWhite;	// = null;
+	public Image						orgImage;
+	public Image						image;
+	public static short			value	= 0;
 
-    Piece(Chessboard chessboard, Player player)
-    {
-        this.chessboard = chessboard;
-        this.player = player;
-        if (player.color == player.color.black)
-        {
-            image = imageBlack;
-        }
-        else
-        {
-            image = imageWhite;
-        }
-        this.name = this.getClass().getSimpleName();
+	Piece(Chessboard chessboard, Player player) {
+		this.chessboard = chessboard;
+		this.player = player;
+		if (player.color == player.color.black) {
+			image = imageBlack;
+		} else {
+			image = imageWhite;
+		}
+		this.name = this.getClass().getSimpleName();
 
-    }
-    /* Method to draw piece on chessboard
-     * @graph : where to draw
-     */
+	}
+	/*
+	 * Method to draw piece on chessboard
+	 * 
+	 * @graph : where to draw
+	 */
 
-    final void draw(Graphics g)
-    {
-        try
-        {
-            Graphics2D g2d = (Graphics2D) g;
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            Point topLeft = this.chessboard.getTopLeftPoint();
-            int height = this.chessboard.get_square_height();
-            int x = (this.square.pozX * height) + topLeft.x;
-            int y = (this.square.pozY * height) + topLeft.y;
-            float addX = (height - image.getWidth(null)) / 2;
-            float addY = (height - image.getHeight(null)) / 2;
-            if (image != null && g != null)
-            {
-                Image tempImage = orgImage;
-                BufferedImage resized = new BufferedImage(height, height, BufferedImage.TYPE_INT_ARGB_PRE);
-                Graphics2D imageGr = (Graphics2D) resized.createGraphics();
-                imageGr.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                imageGr.drawImage(tempImage, 0, 0, height, height, null);
-                imageGr.dispose();
-                image = resized.getScaledInstance(height, height, 0);
-                g2d.drawImage(image, x, y, null);
-            }
-            else
-            {
-                System.out.println("image is null!");
-            }
+	final void draw(Graphics g) {
+		try {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			Point topLeft = this.chessboard.getTopLeftPoint();
+			int height = this.chessboard.get_square_height();
+			int x = (this.square.pozX * height) + topLeft.x;
+			int y = (this.square.pozY * height) + topLeft.y;
+			float addX = (height - image.getWidth(null)) / 2;
+			float addY = (height - image.getHeight(null)) / 2;
+			if (image != null && g != null) {
+				Image tempImage = orgImage;
+				BufferedImage resized = new BufferedImage(height, height, BufferedImage.TYPE_INT_ARGB_PRE);
+				Graphics2D imageGr = (Graphics2D) resized.createGraphics();
+				imageGr.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				imageGr.drawImage(tempImage, 0, 0, height, height, null);
+				imageGr.dispose();
+				image = resized.getScaledInstance(height, height, 0);
+				g2d.drawImage(image, x, y, null);
+			} else {
+				System.out.println("image is null!");
+			}
 
-        }
-        catch (java.lang.NullPointerException exc)
-        {
-            System.out.println("Something wrong when painting piece: " + exc.getMessage());
-        }
-    }
+		} catch (java.lang.NullPointerException exc) {
+			System.out.println("Something wrong when painting piece: " + exc.getMessage());
+		}
+	}
 
-    void clean()
-    {
-    }
+	void clean() {
+	}
 
-    /** method check if Piece can move to given square
-     * @param square square where piece want to move (Square object)
-     * @param allmoves  all moves which can piece do
-     * */
-    boolean canMove(Square square, ArrayList allmoves)
-    {
-        //throw new UnsupportedOperationException("Not supported yet.");
-        ArrayList moves = allmoves;
-        for (Iterator it = moves.iterator(); it.hasNext();)
-        {
-            Square sq = (Square) it.next();//get next from iterator
-            if (sq == square)
-            {//if adress is the same
-                return true; //piece canMove
-            }
-        }
-        return false;//if not, piece cannot move
-    }
+	/** method check if Piece can move to given square
+	 * @param square square where piece want to move (Square object)
+	 * @param allmoves  all moves which can piece do
+	 * */
+	boolean canMove(Square square, ArrayList allmoves) {
+		// throw new UnsupportedOperationException("Not supported yet.");
+		ArrayList moves = allmoves;
+		for (Iterator it = moves.iterator(); it.hasNext();) {
+			Square sq = (Square) it.next();// get next from iterator
+			if (sq == square) {// if adress is the same
+				return true; // piece canMove
+			}
+		}
+		return false;// if not, piece cannot move
+	}
 
-    void setImage()
-    {
-        if (this.player.color == this.player.color.black)
-        {
-            image = imageBlack;
-        }
-        else
-        {
-            image = imageWhite;
-        }
-    }
-    //void setImages(String white, String black) {
-        /* method set image to black or white (depends on player color)
-     * @white: String with name of image with white piece
-     * @black: String with name of image with black piece
-     * */
-    //    this.imageBlack = black;
-    //     this.imageWhite = white;
-    //     if(player.color == player.color.black) {
-    //         this.image = GUI.loadImage(imageBlack);
-    //     } else {
-    //          this.image = GUI.loadImage(imageWhite);
-    //     }
-    //  }/*--endOf-setImages(String white, String black)--*/
+	void setImage() {
+		if (this.player.color == this.player.color.black) {
+			image = imageBlack;
+		} else {
+			image = imageWhite;
+		}
+	}
+	// void setImages(String white, String black) {
+	/*
+	 * method set image to black or white (depends on player color)
+	 * 
+	 * @white: String with name of image with white piece
+	 * 
+	 * @black: String with name of image with black piece
+	 */
+	// this.imageBlack = black;
+	// this.imageWhite = white;
+	// if(player.color == player.color.black) {
+	// this.image = GUI.loadImage(imageBlack);
+	// } else {
+	// this.image = GUI.loadImage(imageWhite);
+	// }
+	// }/*--endOf-setImages(String white, String black)--*/
 
-    abstract public ArrayList allMoves();
+	abstract public ArrayList allMoves();
 
-    /** Method is useful for out of bounds protection
-     * @param x  x position on chessboard
-     * @param y y position on chessboard
-     * @return true if parameters are out of bounds (array)
-     * */
-    protected boolean isout(int x, int y)
-    {
-        if (x < 0 || x > 7 || y < 0 || y > 7)
-        {
-            return true;
-        }
-        return false;
-    }
+	/** Method is useful for out of bounds protection
+	 * @param x  x position on chessboard
+	 * @param y y position on chessboard
+	 * @return true if parameters are out of bounds (array)
+	 * */
+	protected boolean isout(int x, int y) {
+		if (x < 0 || x > 7 || y < 0 || y > 7) {
+			return true;
+		}
+		return false;
+	}
 
-    /** 
-     * @param x y position on chessboard
-     * @param y  y position on chessboard
-     * @return true if can move, false otherwise
-     * */
-    protected boolean checkPiece(int x, int y)
-    {
-        if (chessboard.squares[x][y].piece != null
-                && chessboard.squares[x][y].piece.name.equals("King"))
-        {
-            return false;
-        }
-        Piece piece = chessboard.squares[x][y].piece;
-        if (piece == null || //if this sqhuare is empty
-                piece.player != this.player) //or piece is another player
-        {
-            return true;
-        }
-        return false;
-    }
+	/** 
+	 * @param x y position on chessboard
+	 * @param y  y position on chessboard
+	 * @return true if can move, false otherwise
+	 * */
+	protected boolean checkPiece(int x, int y) {
+		if (chessboard.squares[x][y].piece != null && chessboard.squares[x][y].piece.name.equals("King")) {
+			return false;
+		}
+		Piece piece = chessboard.squares[x][y].piece;
+		if (piece == null || // if this square is empty
+				piece.player != this.player) // or piece is another player
+		{
+			return true;
+		}
+		return false;
+	}
 
-    /** Method check if piece has other owner than calling piece
-     * @param x x position on chessboard
-     * @param y y position on chessboard
-     * @return true if owner(player) is different
-     * */
-    protected boolean otherOwner(int x, int y)
-    {
-        Square sq = chessboard.squares[x][y];
-        if (sq.piece == null)
-        {
-            return false;
-        }
-        if (this.player != sq.piece.player)
-        {
-            return true;
-        }
-        return false;
-    }
+	/** Method check if piece has other owner than calling piece
+	 * @param x x position on chessboard
+	 * @param y y position on chessboard
+	 * @return true if owner(player) is different
+	 * */
+	protected boolean otherOwner(int x, int y) {
+		Square sq = chessboard.squares[x][y];
+		if (sq.piece == null) {
+			return false;
+		}
+		if (this.player != sq.piece.player) {
+			return true;
+		}
+		return false;
+	}
 
-    public String getSymbol()
-    {
-        return this.symbol;
-    }
+	public String getSymbol() {
+		return this.symbol;
+	}
 }
