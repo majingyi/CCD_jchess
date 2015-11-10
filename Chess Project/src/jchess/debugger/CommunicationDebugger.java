aaee717d523c094df2d4a6c75f013ee0ffcbf194
Maurice <maurice.duvigneau@st.ovgu.de>
2015-11-10 10:52:11 +0100

    Simple java debugger for communication logging

---
+++ Chess Project/src/jchess/debugger/CommunicationDebugger.java
@@ -0,0 +1,84 @@
+package jchess.debugger;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.sun.jdi.AbsentInformationException;
+import com.sun.jdi.BooleanValue;
+import com.sun.jdi.IncompatibleThreadStateException;
+import com.sun.jdi.IntegerValue;
+import com.sun.jdi.LocalVariable;
+import com.sun.jdi.Method;
+import com.sun.jdi.StackFrame;
+import com.sun.jdi.Value;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.event.Event;
+import com.sun.jdi.event.EventQueue;
+import com.sun.jdi.event.EventSet;
+import com.sun.jdi.event.MethodEntryEvent;
+import com.sun.jdi.request.EventRequestManager;
+import com.sun.jdi.request.MethodEntryRequest;
+
+public class CommunicationDebugger {
+
+	public static void main(String[] args) {
+
+		try {
+			VMAcquierer acq = new VMAcquierer();
+			VirtualMachine vm = acq.connect(8000);
+
+			addMethodWatch(vm);
+
+			// process events
+			EventQueue eventQueue = vm.eventQueue();
+			while (true) {
+				EventSet eventSet = eventQueue.remove();
+
+				for (Event event : eventSet) {
+					System.out.println("Event happend");
+					System.out.println("Event Class: " + event.getClass());
+
+					if (event instanceof MethodEntryEvent) {
+						MethodEntryEvent entryEvent = (MethodEntryEvent) event;
+						Method m = entryEvent.location().method();
+						if (m.name().startsWith("send")) {
+							List<LocalVariable> vars = m.arguments();
+							for (LocalVariable var : vars) {
+								System.out.println("Argument: " + var.name());
+
+								StackFrame stackFrame = entryEvent.thread().frame(0);
+								Value value = stackFrame.getValue(var);
+								String val = "unknown";
+								if (value instanceof BooleanValue) {
+									val = Boolean.toString(((BooleanValue) value).value());
+								} else if (value instanceof IntegerValue) {
+									val = Integer.toString(((IntegerValue) value).value());
+								}
+
+								System.out.println("Value: " + val);
+							}
+						}
+					}
+				}
+
+				vm.resume();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (AbsentInformationException e) {
+			e.printStackTrace();
+		} catch (IncompatibleThreadStateException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/** Watch all classes of name "Test" */
+	private static void addMethodWatch(VirtualMachine vm) {
+		EventRequestManager erm = vm.eventRequestManager();
+		MethodEntryRequest methodEntryRequest = erm.createMethodEntryRequest();
+		methodEntryRequest.addClassFilter("jchess.server.Table");
+		methodEntryRequest.setEnabled(true);
+	}
+}
--- Chess Project/src/jchess/server/Server.java
+++ Chess Project/src/jchess/server/Server.java
@@ -1,22 +1,20 @@
 /**
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
- * Authors:
- * Mateusz Sławomir Lach ( matlak, msl )
- * Damian Marciniak
+ * Authors: Mateusz Sławomir Lach ( matlak, msl ) Damian Marciniak
  */
 package jchess.server;
 
@@ -35,13 +33,13 @@ public class Server implements Runnable {
 	public static boolean							isPrintEnable	= true;	// print all
 																														// messages (print
 																														// function)
-	public static Map<Integer, Table>	tables;
+	public static Map<Integer, Table>	tables				= null;
 	public static int									port					= 4449;
-	private static ServerSocket				ss;
+	private static ServerSocket				ss						= null;
 	private static boolean						isRunning			= false;
 
 	public Server() {
-		if (!Server.isRunning) // run server if isn't running previous
+		if (Server.isRunning == false) // run server if isn't running previous
 		{
 			runServer();
 
