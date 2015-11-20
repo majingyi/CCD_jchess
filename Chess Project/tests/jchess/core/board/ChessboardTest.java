67eacffff7e9f9f9322fa36cb32c8da5bbf6e48a
Maurice <maurice.duvigneau@st.ovgu.de>
2015-11-20 23:30:02 +0100

    remove side effect

--- Chess Project/tests/jchess/core/board/ChessboardTest.java
+++ Chess Project/tests/jchess/core/board/ChessboardTest.java
@@ -136,120 +136,124 @@ public class ChessboardTest {
 	@Test
 	public void testSetPiecesPlayer1WhiteUpsideDown() throws Exception {
 		Settings.setUpsideDown(true);
-		Chessboard board = new Chessboard(null, null);
+		try {
+			Chessboard board = new Chessboard(null, null);
 
-		Player white = new Player("hans", Player.colors.white);
-		Player black = new Player("wurst", Player.colors.black);
+			Player white = new Player("hans", Player.colors.white);
+			Player black = new Player("wurst", Player.colors.black);
 
-		board.setPieces(Constants.EMPTY_STRING, white, black);
+			board.setPieces(Constants.EMPTY_STRING, white, black);
 
-		Assert.assertNotNull(board.squares[0][0]);
-		Assert.assertNotNull(board.squares[1][0]);
-		Assert.assertNotNull(board.squares[2][0]);
-		Assert.assertNotNull(board.squares[3][0]);
-		Assert.assertNotNull(board.squares[4][0]);
-		Assert.assertNotNull(board.squares[5][0]);
-		Assert.assertNotNull(board.squares[6][0]);
-		Assert.assertNotNull(board.squares[7][0]);
+			Assert.assertNotNull(board.squares[0][0]);
+			Assert.assertNotNull(board.squares[1][0]);
+			Assert.assertNotNull(board.squares[2][0]);
+			Assert.assertNotNull(board.squares[3][0]);
+			Assert.assertNotNull(board.squares[4][0]);
+			Assert.assertNotNull(board.squares[5][0]);
+			Assert.assertNotNull(board.squares[6][0]);
+			Assert.assertNotNull(board.squares[7][0]);
 
-		Assert.assertTrue((board.squares[0][0].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[1][0].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[2][0].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[3][0].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[4][0].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[5][0].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[6][0].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[7][0].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[0][0].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[1][0].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[2][0].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[3][0].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[4][0].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[5][0].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[6][0].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[7][0].piece.player.color == Player.colors.white));
 
-		Assert.assertTrue((board.squares[0][0].piece.getSymbol() == Rook.SYMBOL));
-		Assert.assertTrue((board.squares[1][0].piece.getSymbol() == Knight.SYMBOL));
-		Assert.assertTrue((board.squares[2][0].piece.getSymbol() == Bishop.SYMBOL));
-		Assert.assertTrue((board.squares[3][0].piece.getSymbol() == King.SYMBOL));
-		Assert.assertTrue((board.squares[4][0].piece.getSymbol() == Queen.SYMBOL));
-		Assert.assertTrue((board.squares[5][0].piece.getSymbol() == Bishop.SYMBOL));
-		Assert.assertTrue((board.squares[6][0].piece.getSymbol() == Knight.SYMBOL));
-		Assert.assertTrue((board.squares[7][0].piece.getSymbol() == Rook.SYMBOL));
+			Assert.assertTrue((board.squares[0][0].piece.getSymbol() == Rook.SYMBOL));
+			Assert.assertTrue((board.squares[1][0].piece.getSymbol() == Knight.SYMBOL));
+			Assert.assertTrue((board.squares[2][0].piece.getSymbol() == Bishop.SYMBOL));
+			Assert.assertTrue((board.squares[3][0].piece.getSymbol() == King.SYMBOL));
+			Assert.assertTrue((board.squares[4][0].piece.getSymbol() == Queen.SYMBOL));
+			Assert.assertTrue((board.squares[5][0].piece.getSymbol() == Bishop.SYMBOL));
+			Assert.assertTrue((board.squares[6][0].piece.getSymbol() == Knight.SYMBOL));
+			Assert.assertTrue((board.squares[7][0].piece.getSymbol() == Rook.SYMBOL));
 
-		Assert.assertNotNull(board.squares[0][1]);
-		Assert.assertNotNull(board.squares[1][1]);
-		Assert.assertNotNull(board.squares[2][1]);
-		Assert.assertNotNull(board.squares[3][1]);
-		Assert.assertNotNull(board.squares[4][1]);
-		Assert.assertNotNull(board.squares[5][1]);
-		Assert.assertNotNull(board.squares[6][1]);
-		Assert.assertNotNull(board.squares[7][1]);
+			Assert.assertNotNull(board.squares[0][1]);
+			Assert.assertNotNull(board.squares[1][1]);
+			Assert.assertNotNull(board.squares[2][1]);
+			Assert.assertNotNull(board.squares[3][1]);
+			Assert.assertNotNull(board.squares[4][1]);
+			Assert.assertNotNull(board.squares[5][1]);
+			Assert.assertNotNull(board.squares[6][1]);
+			Assert.assertNotNull(board.squares[7][1]);
 
-		Assert.assertTrue((board.squares[0][1].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[1][1].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[2][1].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[3][1].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[4][1].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[5][1].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[6][1].piece.player.color == Player.colors.white));
-		Assert.assertTrue((board.squares[7][1].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[0][1].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[1][1].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[2][1].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[3][1].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[4][1].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[5][1].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[6][1].piece.player.color == Player.colors.white));
+			Assert.assertTrue((board.squares[7][1].piece.player.color == Player.colors.white));
 
-		Assert.assertTrue((board.squares[0][1].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[1][1].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[2][1].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[3][1].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[4][1].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[5][1].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[6][1].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[7][1].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[0][1].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[1][1].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[2][1].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[3][1].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[4][1].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[5][1].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[6][1].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[7][1].piece.getSymbol() == Pawn.SYMBOL));
 
-		Assert.assertNotNull(board.squares[0][6]);
-		Assert.assertNotNull(board.squares[1][6]);
-		Assert.assertNotNull(board.squares[2][6]);
-		Assert.assertNotNull(board.squares[3][6]);
-		Assert.assertNotNull(board.squares[4][6]);
-		Assert.assertNotNull(board.squares[5][6]);
-		Assert.assertNotNull(board.squares[6][6]);
-		Assert.assertNotNull(board.squares[7][6]);
+			Assert.assertNotNull(board.squares[0][6]);
+			Assert.assertNotNull(board.squares[1][6]);
+			Assert.assertNotNull(board.squares[2][6]);
+			Assert.assertNotNull(board.squares[3][6]);
+			Assert.assertNotNull(board.squares[4][6]);
+			Assert.assertNotNull(board.squares[5][6]);
+			Assert.assertNotNull(board.squares[6][6]);
+			Assert.assertNotNull(board.squares[7][6]);
 
-		Assert.assertTrue((board.squares[0][6].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[1][6].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[2][6].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[3][6].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[4][6].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[5][6].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[6][6].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[7][6].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[0][6].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[1][6].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[2][6].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[3][6].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[4][6].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[5][6].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[6][6].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[7][6].piece.player.color == Player.colors.black));
 
-		Assert.assertTrue((board.squares[0][6].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[1][6].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[2][6].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[3][6].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[4][6].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[5][6].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[6][6].piece.getSymbol() == Pawn.SYMBOL));
-		Assert.assertTrue((board.squares[7][6].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[0][6].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[1][6].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[2][6].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[3][6].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[4][6].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[5][6].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[6][6].piece.getSymbol() == Pawn.SYMBOL));
+			Assert.assertTrue((board.squares[7][6].piece.getSymbol() == Pawn.SYMBOL));
 
-		Assert.assertNotNull(board.squares[0][7]);
-		Assert.assertNotNull(board.squares[1][7]);
-		Assert.assertNotNull(board.squares[2][7]);
-		Assert.assertNotNull(board.squares[3][7]);
-		Assert.assertNotNull(board.squares[4][7]);
-		Assert.assertNotNull(board.squares[5][7]);
-		Assert.assertNotNull(board.squares[6][7]);
-		Assert.assertNotNull(board.squares[7][7]);
+			Assert.assertNotNull(board.squares[0][7]);
+			Assert.assertNotNull(board.squares[1][7]);
+			Assert.assertNotNull(board.squares[2][7]);
+			Assert.assertNotNull(board.squares[3][7]);
+			Assert.assertNotNull(board.squares[4][7]);
+			Assert.assertNotNull(board.squares[5][7]);
+			Assert.assertNotNull(board.squares[6][7]);
+			Assert.assertNotNull(board.squares[7][7]);
 
-		Assert.assertTrue((board.squares[0][7].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[1][7].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[2][7].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[3][7].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[4][7].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[5][7].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[6][7].piece.player.color == Player.colors.black));
-		Assert.assertTrue((board.squares[7][7].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[0][7].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[1][7].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[2][7].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[3][7].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[4][7].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[5][7].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[6][7].piece.player.color == Player.colors.black));
+			Assert.assertTrue((board.squares[7][7].piece.player.color == Player.colors.black));
 
-		Assert.assertTrue((board.squares[0][7].piece.getSymbol() == Rook.SYMBOL));
-		Assert.assertTrue((board.squares[1][7].piece.getSymbol() == Knight.SYMBOL));
-		Assert.assertTrue((board.squares[2][7].piece.getSymbol() == Bishop.SYMBOL));
-		Assert.assertTrue((board.squares[3][7].piece.getSymbol() == King.SYMBOL));
-		Assert.assertTrue((board.squares[4][7].piece.getSymbol() == Queen.SYMBOL));
-		Assert.assertTrue((board.squares[5][7].piece.getSymbol() == Bishop.SYMBOL));
-		Assert.assertTrue((board.squares[6][7].piece.getSymbol() == Knight.SYMBOL));
-		Assert.assertTrue((board.squares[7][7].piece.getSymbol() == Rook.SYMBOL));
+			Assert.assertTrue((board.squares[0][7].piece.getSymbol() == Rook.SYMBOL));
+			Assert.assertTrue((board.squares[1][7].piece.getSymbol() == Knight.SYMBOL));
+			Assert.assertTrue((board.squares[2][7].piece.getSymbol() == Bishop.SYMBOL));
+			Assert.assertTrue((board.squares[3][7].piece.getSymbol() == King.SYMBOL));
+			Assert.assertTrue((board.squares[4][7].piece.getSymbol() == Queen.SYMBOL));
+			Assert.assertTrue((board.squares[5][7].piece.getSymbol() == Bishop.SYMBOL));
+			Assert.assertTrue((board.squares[6][7].piece.getSymbol() == Knight.SYMBOL));
+			Assert.assertTrue((board.squares[7][7].piece.getSymbol() == Rook.SYMBOL));
+		} finally {
+			Settings.setUpsideDown(false);
+		}
 	}
 
 	@Test
