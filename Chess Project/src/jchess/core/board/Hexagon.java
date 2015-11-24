acbeebdf2bb3b6d9b3bc32f73291368e670b4178
Maurice <maurice.duvigneau@st.ovgu.de>
2015-11-24 21:41:27 +0100

    Introduce hexagon field

---
+++ Chess Project/src/jchess/core/board/Hexagon.java
@@ -0,0 +1,9 @@
+package jchess.core.board;
+
+public class Hexagon extends ChessboardField {
+
+	public Hexagon(String identifier, Chessboard chessboard) {
+		super(identifier, chessboard);
+	}
+
+}
