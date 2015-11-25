b75db4d52bc95006746a7354148ed690238526be
Maurice <maurice.duvigneau@st.ovgu.de>
2015-11-25 22:14:12 +0100

    remove obsolete tests

--- Chess_Project/tests/jchess/AllTests.java
+++ Chess_Project/tests/jchess/AllTests.java
@@ -1,6 +1,5 @@
 package jchess;
 
-import jchess.core.board.ChessboardTest;
 import jchess.core.board.graph.GraphEdgeTest;
 import jchess.core.board.graph.HexagonChessFieldGraphInitializerTest;
 import jchess.core.pieces.PawnTest;
@@ -14,8 +13,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ HexagonChessFieldGraphInitializerTest.class, GraphEdgeTest.class, SettingsTest.class, ChessboardTest.class, PawnTest.class, PieceTest.class,
-		ThemeTest.class, ClockTest.class })
+@SuiteClasses({ HexagonChessFieldGraphInitializerTest.class, GraphEdgeTest.class, SettingsTest.class, PawnTest.class, PieceTest.class, ThemeTest.class,
+		ClockTest.class })
 public class AllTests {
 
 }
--- Chess_Project/tests/jchess/core/pieces/PawnTest.java
+++ Chess_Project/tests/jchess/core/pieces/PawnTest.java
@@ -1,14 +1,7 @@
 package jchess.core.pieces;
 
-import java.util.ArrayList;
-
 import jchess.core.board.Chessboard;
-import jchess.core.board.ChessboardField;
-import jchess.core.board.Square;
-import jchess.core.util.Constants;
 import jchess.core.util.Player;
-import jchess.core.util.Settings;
-import jchess.ui.GameTab;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -71,85 +64,6 @@ public class PawnTest {
 		Assert.assertEquals(Rook.SYMBOL, p.getSymbol());
 		Assert.assertTrue(p.getMoveBehaviorClass().equals(RookMoveBehavior.class));
 		Assert.assertEquals("R", p.getSymbolForMoveHistory());
-	}
-
-	@Test
-	public void testAllMoves() throws Exception {
-
-		Settings.setWhitePlayersName("Hans");
-		Settings.setBlackPlayersName("Wurst");
-
-		GameTab game = new GameTab();
-		Chessboard board = game.getChessboard();
-
-		board.setPieces(Constants.EMPTY_STRING, game.getWhitePlayer(), game.getBlackPlayer());
-
-		// first move
-		Pawn p = (Pawn) board.getFields()[1][1].getPiece();
-		ArrayList<ChessboardField> possibleMoves = p.allMoves();
-		Assert.assertEquals(possibleMoves.size(), 2);
-
-		Square first = (Square) possibleMoves.get(0);
-		Square second = (Square) possibleMoves.get(1);
-
-		Assert.assertEquals(first.pozX, 1);
-		Assert.assertEquals(first.pozY, 2);
-		Assert.assertEquals(second.pozX, 1);
-		Assert.assertEquals(second.pozY, 3);
-
-		// second move
-		board.move(board.getFields()[1][1], second);
-
-		Square newSquare = (Square) p.getField();
-		Assert.assertEquals(1, newSquare.pozX);
-		Assert.assertEquals(3, newSquare.pozY);
-
-		possibleMoves = p.allMoves();
-		Assert.assertEquals(possibleMoves.size(), 1);
-		first = (Square) possibleMoves.get(0);
-
-		Assert.assertEquals(first.pozX, 1);
-		Assert.assertEquals(first.pozY, 4);
-
-		// blocked
-		board.move(board.getFields()[1][6], board.getFields()[1][4]);
-		possibleMoves = p.allMoves();
-		Assert.assertEquals(0, possibleMoves.size());
-
-		// pawn to capture
-		board.move(board.getFields()[2][1], board.getFields()[2][3]);
-		possibleMoves = board.getFields()[2][3].getPiece().allMoves();
-		Assert.assertEquals(2, possibleMoves.size());
-		first = (Square) possibleMoves.get(0);
-		Assert.assertEquals(first.pozX, 2);
-		Assert.assertEquals(first.pozY, 4);
-		second = (Square) possibleMoves.get(1);
-		Assert.assertEquals(second.pozX, 1);
-		Assert.assertEquals(second.pozY, 4);
-
-		board.move(board.getFields()[2][6], board.getFields()[2][4]);
-		possibleMoves = p.allMoves();
-		Assert.assertEquals(1, possibleMoves.size());
-		first = (Square) possibleMoves.get(0);
-		Assert.assertEquals(first.pozX, 2);
-		Assert.assertEquals(first.pozY, 4);
-
-		possibleMoves = board.getFields()[2][3].getPiece().allMoves();
-		Assert.assertEquals(1, possibleMoves.size());
-		first = (Square) possibleMoves.get(0);
-		Assert.assertEquals(first.pozX, 1);
-		Assert.assertEquals(first.pozY, 4);
-
-		// wrong en passante
-		board.move(board.getFields()[5][6], board.getFields()[5][4]);
-		board.move(board.getFields()[4][1], board.getFields()[4][3]);
-		board.move(board.getFields()[5][4], board.getFields()[5][3]);
-
-		possibleMoves = board.getFields()[4][3].getPiece().allMoves();
-		Assert.assertEquals(1, possibleMoves.size());
-		first = (Square) possibleMoves.get(0);
-		Assert.assertEquals(first.pozX, 4);
-		Assert.assertEquals(first.pozY, 4);
 	}
 
 	private Pawn createPawn() throws Exception {
