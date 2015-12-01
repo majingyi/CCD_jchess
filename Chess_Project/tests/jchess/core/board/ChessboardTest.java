6c8d3bd2417d4c8b53832780f93e51162343e46d
Maurice <maurice.duvigneau@st.ovgu.de>
2015-12-01 19:26:26 +0100

    Add Tests for testing addKing and GetKingForColor

--- Chess_Project/tests/jchess/core/board/ChessboardTest.java
+++ Chess_Project/tests/jchess/core/board/ChessboardTest.java
@@ -1,17 +1,63 @@
 package jchess.core.board;
 
+import jchess.core.pieces.King;
 import jchess.core.util.Game;
+import jchess.core.util.Player;
+import jchess.core.util.Player.colors;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ChessboardTest {
 
-	// @Test
-	// public void testGetKingForColor() throws Exception {
-	// throw new RuntimeException("not yet implemented");
-	// }
-	//
+	@Test
+	public void testGetKingForColor() throws Exception {
+		Chessboard board = new Chessboard(null, null);
+		Game game = new Game(board, null);
+		game.startNewGame();
+
+		King red = board.getKingForColor(colors.red);
+		Assert.assertNotNull(red);
+		Assert.assertEquals(colors.red, red.getPlayer().getColor());
+
+		King black = board.getKingForColor(colors.black);
+		Assert.assertNotNull(black);
+		Assert.assertEquals(colors.black, black.getPlayer().getColor());
+
+		King white = board.getKingForColor(colors.white);
+		Assert.assertNotNull(white);
+		Assert.assertEquals(colors.white, white.getPlayer().getColor());
+
+		King nothing = board.getKingForColor(null);
+		Assert.assertNull(nothing);
+	}
+
+	@Test
+	public void testAddKing() throws Exception {
+		Chessboard board = new Chessboard(null, null);
+
+		Player red = new Player("hdwf", colors.red);
+		board.addKing(new King(board, red, null));
+
+		Player black = new Player("hdwf", colors.black);
+		board.addKing(new King(board, black, null));
+
+		Player white = new Player("hdwf", colors.white);
+		board.addKing(new King(board, white, null));
+
+		boolean exception = false;
+		try {
+			red = new Player("hdwf", colors.red);
+			board.addKing(new King(board, red, null));
+		} catch (Exception e) {
+			Assert.assertEquals("King with color red is already existing on this borad.", e.getMessage());
+			exception = true;
+		}
+
+		Assert.assertTrue(exception);
+
+	}
+
 	// @Test
 	// public void testGetActiveField() throws Exception {
 	// throw new RuntimeException("not yet implemented");
