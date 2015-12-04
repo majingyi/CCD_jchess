f4d515407bc929ada86b19a494b64dba14009c1c
Serhii <mahghrom@gmail.com>
2015-12-04 22:20:15 +0100

    Castling for KingMoveBehavior.

--- Chess_Project/src/jchess/core/pieces/KingMoveBehavior.java
+++ Chess_Project/src/jchess/core/pieces/KingMoveBehavior.java
@@ -1,9 +1,12 @@
 package jchess.core.pieces;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import jchess.core.board.Chessboard;
 import jchess.core.board.ChessboardField;
+import jchess.core.board.graph.DirectedGraphEdge.EdgeDirection;
+import jchess.core.board.graph.GraphEdge.EdgeType;
 import jchess.core.util.Player;
 
 public class KingMoveBehavior extends MoveBehavior {
@@ -19,109 +22,76 @@ public class KingMoveBehavior extends MoveBehavior {
 	public ArrayList<ChessboardField> allMoves() throws Exception {
 
 		ArrayList<ChessboardField> allMoves = new ArrayList<ChessboardField>();
+		Player.PlayerColor color = this.m_Player.getColor();
 
 		// adding all straight fields
 		for (ChessboardField straightField : this.m_Chessboard.getStraightFields(this.m_Field, 1, this.m_Player.getColor())) {
 
-			if (this.m_Chessboard.getKingForColor(this.m_Player.getColor()).isSafe(straightField)) {
+			if (this.m_Chessboard.getKingForColor(color).isSafe(straightField)) {
 				allMoves.add(straightField);
 			} else {
 				continue;
 			}
 		}
+
 		// adding all diagonal fields
 		for (ChessboardField diagonalField : this.m_Chessboard.getDiagonalFields(this.m_Field, 1, this.m_Player.getColor())) {
 
-			if (this.m_Chessboard.getKingForColor(this.m_Player.getColor()).isSafe(diagonalField)) {
+			if (this.m_Chessboard.getKingForColor(color).isSafe(diagonalField)) {
 				allMoves.add(diagonalField);
 			} else {
 				continue;
 			}
 		}
 
-		// ChessboardField sq;
-		// ChessboardField sq1;
-		// for (int i = this.m_Field.pozX - 1; i <= this.m_Field.pozX + 1; i++) {
-		// for (int y = this.m_Field.pozY - 1; y <= this.m_Field.pozY + 1; y++) {
-		// if (Chessboard.isout(i, y) == false) {// out of bounds protection
-		// sq = this.m_Chessboard.getFields()[i][y];
-		// if (this.m_Field == sq) {// if we're checking square on which is King
-		// continue;
-		// }
-		// if (this.checkPiece(i, y)) {// if square is empty
-		// if (king.isSafe(sq)) {
-		// list.add(sq);
-		// }
-		// }
-		// }
-		// }
-		// }
-		//
-		// if (!king.wasMotion && !king.isChecked()) {// check if king was not moved
-		// // before
-		//
-		// if (m_Chessboard.getFields()[0][this.m_Field.pozY].getPiece() != null
-		// && m_Chessboard.getFields()[0][this.m_Field.pozY].getPiece().getSymbol()
-		// == Rook.SYMBOL) {
-		// boolean canCastling = true;
-		//
-		// Rook rook = (Rook)
-		// m_Chessboard.getFields()[0][this.m_Field.pozY].getPiece();
-		// if (!rook.wasMotion) {
-		// for (int i = this.m_Field.pozX - 1; i > 0; i--) {// go left
-		// if (m_Chessboard.getFields()[i][this.m_Field.pozY].getPiece() != null) {
-		// canCastling = false;
-		// break;
-		// }
-		// }
-		// sq = this.m_Chessboard.getFields()[this.m_Field.pozX -
-		// 2][this.m_Field.pozY];
-		// sq1 = this.m_Chessboard.getFields()[this.m_Field.pozX -
-		// 1][this.m_Field.pozY];
-		// if (canCastling && king.isSafe(sq) && king.isSafe(sq1)) { // can do
-		// // castling
-		// // when none
-		// // of Sq,sq1
-		// // is
-		// // checked
-		// list.add(sq);
-		// }
-		// }
-		// }
-		// if (m_Chessboard.getFields()[7][this.m_Field.pozY].getPiece() != null
-		// && m_Chessboard.getFields()[7][this.m_Field.pozY].getPiece().getSymbol()
-		// == Rook.SYMBOL) {
-		// boolean canCastling = true;
-		// Rook rook = (Rook)
-		// m_Chessboard.getFields()[7][this.m_Field.pozY].getPiece();
-		// if (!rook.wasMotion) {// if king was not moves before and is not checked
-		// for (int i = this.m_Field.pozX + 1; i < 7; i++) {// go right
-		// if (m_Chessboard.getFields()[i][this.m_Field.pozY].getPiece() != null)
-		// {// if
-		// // square
-		// // is
-		// // not
-		// // empty
-		// canCastling = false;// cannot castling
-		// break; // exit
-		// }
-		// }
-		// sq = this.m_Chessboard.getFields()[this.m_Field.pozX +
-		// 2][this.m_Field.pozY];
-		// sq1 = this.m_Chessboard.getFields()[this.m_Field.pozX +
-		// 1][this.m_Field.pozY];
-		// if (canCastling && king.isSafe(sq) && king.isSafe(sq1)) {// can do
-		// // castling
-		// // when none
-		// // of Sq,sq1
-		// // is
-		// // checked
-		// list.add(sq);
-		// }
-		// }
-		// }
-		// }
+		// checking possibility for castling
+		EdgeDirection toLeft = this.m_Chessboard.getDirectionFromPlayersPOV(color, EdgeDirection.LEFT);
+		EdgeDirection toRight = this.m_Chessboard.getDirectionFromPlayersPOV(color, EdgeDirection.RIGHT);
+
+		// if that's the first move of the king and it's not checked
+		if (this.king.wasMotion == false && this.king.isChecked() == false) {
+
+			List<Rook> rooks = this.m_Chessboard.getRooksForColor(color);
+			// leftside castling (long)
+			for (ChessboardField field = this.m_Field; field == rooks.get(0).getField()
+					|| field == rooks.get(1).getField(); field = field.getNextField(toLeft, EdgeType.STRAIGHT)) {
+				if (field.getPiece() != null) {
+					break;
+				}
+				if ((field == rooks.get(0).getField() && rooks.get(0).wasMotion == false) || (field == rooks.get(1).getField() && rooks.get(1).wasMotion == false)) {
+					allMoves.add(field);
+				}
+			}
+			for (ChessboardField field = this.m_Field; field == rooks.get(0).getField()
+					|| field == rooks.get(1).getField(); field = field.getNextField(toRight, EdgeType.STRAIGHT)) {
+				if (field.getPiece() != null) {
+					break;
+				}
+				if ((field == rooks.get(0).getField() && rooks.get(0).wasMotion == false) || (field == rooks.get(1).getField() && rooks.get(1).wasMotion == false)) {
+					allMoves.add(field);
+				}
+			}
+			if (true) { // if that's the first move of left rook
+				// three fields to the left has to be without pieces
+				ChessboardField field1 = this.m_Field.getNextField(toLeft, EdgeType.STRAIGHT);
+				ChessboardField field2 = field1.getNextField(toLeft, EdgeType.STRAIGHT);
+				ChessboardField field3 = field2.getNextField(toLeft, EdgeType.STRAIGHT);
+				if (field1.getPiece() == null && field2.getPiece() == null && field3.getPiece() == null) {
+					allMoves.add(field2);
+				}
+			}
+
+			// rightside castling (short)
+			if (true) { // if that's the first move of right rook
+				// two fields to the right has to be without pieces
+				ChessboardField field1 = this.m_Field.getNextField(toRight, EdgeType.STRAIGHT);
+				ChessboardField field2 = field1.getNextField(toRight, EdgeType.STRAIGHT);
+				if (field1.getPiece() == null && field2.getPiece() == null) {
+					allMoves.add(field2);
+				}
+			}
+		}
+
 		return allMoves;
 	}
-
 }
