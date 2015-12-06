54c6ca526cf2d6826c0cd2d81e450f9f2d6710ef
Serhii <mahghrom@gmail.com>
2015-12-06 17:30:52 +0100

    New test for BishopMoveBehavior. Test for RookMoveBehavior is updated.

--- Chess_Project/tests/jchess/core/pieces/BishopMoveBehaviorTest.java
+++ Chess_Project/tests/jchess/core/pieces/BishopMoveBehaviorTest.java
@@ -12,27 +12,35 @@ import jchess.core.util.Player;
 import jchess.core.util.Player.PlayerColor;
 import junit.framework.Assert;
 
-public class RookMoveBehaviorTest {
+public class BishopMoveBehaviorTest {
 
 	@Test
 	public void testAllMoves() throws Exception {
-		// Rook in the center
+		// Bishop in the center
 		Player playerl = new Player("redPl", PlayerColor.RED);
 		Player player2 = new Player("blackPl", PlayerColor.BLACK);
 		Chessboard board = new Chessboard(null, null);
 
 		// friend pawn on the way
 		Pawn p1 = new Pawn(board, playerl, null);
-		board.getField("G7").setPiece(p1);
-		// foe pawn on the way
-		Pawn p2 = new Pawn(board, player2, null);
+		board.getField("G5").setPiece(p1);
+		// friend pawn blocks only 1 of 2 straight fields on the way
+		Pawn p2 = new Pawn(board, playerl, null);
 		board.getField("E5").setPiece(p2);
+		// two foe pawns on the way block both straight fields on the way
+		Pawn p3 = new Pawn(board, player2, null);
+		board.getField("C1").setPiece(p3);
+		Pawn p4 = new Pawn(board, player2, null);
+		board.getField("C2").setPiece(p4);
+		// foe pawn on the way
+		Pawn p5 = new Pawn(board, player2, null);
+		board.getField("B7").setPiece(p4);
+
 		// calculating allowed moves
-		RookMoveBehavior rmb = new RookMoveBehavior(playerl, board, board.getField("G5"));
+		BishopMoveBehavior bmb = new BishopMoveBehavior(playerl, board, board.getField("F3"));
 
 		// creating array list of expected fields
-		String[] expectedIdentifiers = new String[] { "G4", "G3", "G2", "G1", "H5", "I5", "J5", "K5", "H6", "I7", "J8", "K9", "L10", "M11", "G6", "F5", "E5", "F4",
-				"E3", "D2", "C1" };
+		String[] expectedIdentifiers = new String[] { "G2", "H4", "J5", "E4", "D5", "C6", "B7", "D2", "E1" };
 		ArrayList<ChessboardField> expectedFields = new ArrayList<ChessboardField>();
 		for (String expID : expectedIdentifiers) {
 			expectedFields.add(board.getField(expID));
@@ -44,15 +52,15 @@ public class RookMoveBehaviorTest {
 				return field1.getIdentifier().compareTo(field2.getIdentifier());
 			}
 		});
-		Collections.sort(rmb.allMoves(), new Comparator<ChessboardField>() {
+		Collections.sort(bmb.allMoves(), new Comparator<ChessboardField>() {
 			public int compare(ChessboardField field1, ChessboardField field2) {
 				return field1.getIdentifier().compareTo(field2.getIdentifier());
 			}
 		});
 
 		// comparison
-		Assert.assertEquals(expectedFields, rmb.allMoves());
-
+		Assert.assertEquals(expectedFields, bmb.allMoves());
+		;
 	}
 
 }
